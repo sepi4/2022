165b01dbe07fb4adde550a2c54212eb2908b2174
sepi4 <polser1@gmail.com>
2023-01-26 01:35:58 +0200

    16 don't know

--- 2022/16/16-01.ts
+++ 2022/16/16-01.ts
@@ -2,17 +2,31 @@ import { readInput } from "../../lib"
 
 export { }
 
+function getExampleInput() {
+  return `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II`
+}
+
+type Tunnels = {
+  [key: string]: Tunnel
+}
+
 class Tunnel {
   public name: string
   public rate: number
   public arr: string[]
   public isOpen: boolean = false
-  public notUsed: boolean = true
 
   public leadsTo: Tunnel[] = []
 
-  public flow: number = 0
-  public fromStack: Tunnel[] = []
 
   constructor(name: string, rate: number, arr: string[]) {
     this.name = name
@@ -20,14 +34,15 @@ class Tunnel {
     this.arr = arr
   }
 
-  public initLeadsTo(tunnels: Map<string, Tunnel>) {
+  public initLeadsTo(tunnels: Tunnels) {
     for (let tunnelName of this.arr) {
-      let t = tunnels.get(tunnelName)
+      let t = tunnels[tunnelName];
       if (t) {
         this.leadsTo.push(t)
       }
     }
   }
+
 }
 
 function parseInput(row: string) {
@@ -47,95 +62,63 @@ function parseInput(row: string) {
   throw new Error("Error in row");
 }
 
-function getExampleInput() {
-  return `Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-Valve BB has flow rate=13; tunnels lead to valves CC, AA
-Valve CC has flow rate=2; tunnels lead to valves DD, BB
-Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-Valve EE has flow rate=3; tunnels lead to valves FF, DD
-Valve FF has flow rate=0; tunnels lead to valves EE, GG
-Valve GG has flow rate=0; tunnels lead to valves FF, HH
-Valve HH has flow rate=22; tunnel leads to valve GG
-Valve II has flow rate=0; tunnels lead to valves AA, JJ
-Valve JJ has flow rate=21; tunnel leads to valve II`
-}
+type OpenEdges = {
+  [key: string]: number,
+};
 
-function calc(
-  tunnels: Map<string, Tunnel>,
-  currentName: string,
+function goIn(
+  name: string,
+  tunnels: Tunnels,
   timeLeft: number,
-  sum: number,
-) {
+  openEdges: OpenEdges,
+): OpenEdges | undefined {
   if (timeLeft === 0) {
-    return sum
+    // console.log(openEdges, timeLeft, name);
+    return openEdges;
   }
+  // console.log(name, timeLeft, openEdges);
+  let current = tunnels[name];
 
-  let current = tunnels.get(currentName)!
-  let arr = current.leadsTo
-  let sums: number[] = []
-
-  if (!current.isOpen && current.rate > 0) {
-    current.isOpen = true
-    sums.push(calc(
-      tunnels,
+  let sums = [];
+  if (!openEdges[current.name] && current.rate > 0) {
+     goIn(
       current.name,
-      timeLeft - 1,
-      sum,
-    ))
-  } else if (current.notUsed) {
-    // console.log(currentName, timeLeft, sum)
-    sum += timeLeft * current.rate
-    current.notUsed = false
-    console.log(currentName, timeLeft, sum)
-
-  }
-  for (let t of arr) {
-    sums.push(calc(
       tunnels,
-      t.name,
       timeLeft - 1,
-      sum,
-    ))
+      { ...openEdges, [current.name]: timeLeft - 1 }
+    );
+  } else {
+    for (let t of current.leadsTo) {
+      goIn(
+        t.name,
+        tunnels,
+        timeLeft - 1,
+        { ...openEdges }
+      );
+    }
   }
-  return Math.max(...sums)
 }
 
 async function main() {
+  // INIT ==================
+
   // let input = await readInput('./2022/16/input.txt')
   let input = getExampleInput()
   let rows = input.split('\n')
   let arr = rows.map(r => parseInput(r))
-  let tunnels: Map<string, Tunnel> = new Map()
+  let tunnels: Tunnels = {};
   for (let a of arr) {
     const t = new Tunnel(a.name, a.rate, a.arr)
-    tunnels.set(a.name, t);
+    tunnels[a.name] = t;
   }
-  for (let t of tunnels.values()) {
-    t.initLeadsTo(tunnels)
-  }
-  let time = 5;
-  let sum = 0;
-  let current = tunnels.get('AA')!
-  console.log(current)
-
-  // let result = calc(
-  //   tunnels,
-  //   current.name,
-  //   time,
-  //   sum
-  // )
-  // console.log('result:', result)
-
-  let stack: Tunnel[] = []
-  stack.push(current)
-  for (let timeLeft = 30; timeLeft > 0; timeLeft--) {
-    while (stack.length) {
-      let c = stack.pop()!
-    }
+  for (let t of Object.values(tunnels)) {
+    t.initLeadsTo(tunnels);
   }
 
+  // MAIN ==================
+  // console.log(tunnels);
+  goIn('AA', tunnels, 25, {});
 
 }
+
 main()
-
-
