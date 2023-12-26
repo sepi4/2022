c70ab4c5fb1080cfd7385b682ddd308d5b0b84f6
Sergei Polishsuk <serpo@eg.fi>
2023-12-26 03:48:55 +0200

    2023, c#, 11, part1

--- 2023/c#/11/Program.cs
+++ 2023/c#/11/Program.cs
@@ -1,19 +1,121 @@
-﻿public class Program
+﻿using AdventCodeLibrary;
+
+public class Program
 {
   public static void Main(string[] args)
   {
-
-    Console.WriteLine("Hello, World!");
+    var file = "example.txt";
+    var path = $"/Users/serpo/tutorials/advent-of-code/2023/c#/11/{file}";
+    var input = AdventCode.ReadInput(path);
+    Console.WriteLine(CalculateDistanceSum(input));
   }
 
-  public static int Sum2(int a, int b)
+  public static int CalculateDistanceSum(string[] input)
   {
-    return a + b;
+    var expandedSpace = ExpandSpace(input);
+    var galaxies = GetGalaxies(expandedSpace);
+    var pairs = new List<(Galaxy, Galaxy)>();
+    for (var i = 0; i < galaxies.Count - 1; i++)
+    {
+      for (var j = i + 1; j < galaxies.Count; j++)
+      {
+        pairs.Add((galaxies[i], galaxies[j]));
+      }
+    }
+    var distSum = pairs.Aggregate(0, (pre, pair) =>
+    {
+      var (a, b) = pair;
+      return pre + GetDist(a, b);
+    });
+    return distSum;
   }
 
-  public static string[] ReadInput(string path)
+  public static int GetDist(Galaxy a, Galaxy b)
   {
-    var input = File.ReadAllText(path);
-    return input.Split("\n");
+    return Math.Abs(a.Y - b.Y) + Math.Abs(a.X - b.X);
   }
+
+  public static List<Galaxy> GetGalaxies(string[] space)
+  {
+    var galaxies = new List<Galaxy>();
+    for (var i = 0; i < space.Length; i++)
+    {
+      for (var j = 0; j < space[i].Length; j++)
+      {
+        if (space[i][j] == '#')
+        {
+          galaxies.Add(new Galaxy { Y = i, X = j });
+        }
+      }
+    }
+
+    return galaxies;
+  }
+
+  public static string[] ExpandSpace(string[] space)
+  {
+    var list = space.Select(s => s.ToList()).ToList();
+    var (emptyRowIndexes, emptyColumnIndexes) = GetEmptyIndexes(list);
+
+    // rows
+    var rowIndexOffset = 0;
+    foreach (var rowIndex in emptyRowIndexes)
+    {
+      list.Insert(rowIndex + rowIndexOffset, Enumerable.Repeat('.', list[0].Count).ToList());
+      rowIndexOffset++;
+    }
+
+    // columns
+    var newList = new List<List<char>>();
+    foreach (var row in list)
+    {
+      var newRow = row.ToList();
+      var columnIndexOffset = 0;
+      foreach (var columnIndex in emptyColumnIndexes)
+      {
+        newRow.Insert(columnIndex + columnIndexOffset++, '.');
+      }
+
+      newList.Add(newRow);
+    }
+
+    return newList.Select(row => string.Join("", row)).ToArray();
+  }
+
+  public static (List<int>, List<int>) GetEmptyIndexes(List<List<char>> list)
+  {
+    // go through each row
+    var emptyRowIndexes = new List<int>();
+    for (var i = 0; i < list.Count; i++)
+    {
+      if (AreAllEmpty(list[i]))
+      {
+        emptyRowIndexes.Add(i);
+      }
+    }
+
+    // go through each column
+    var emptyColumnIndexes = new List<int>();
+    for (var i = 0; i < list[0].Count; i++)
+    {
+      var column = list.Select(row => row[i]).ToList();
+      if (AreAllEmpty(column))
+      {
+        emptyColumnIndexes.Add(i);
+      }
+    }
+
+    return (emptyRowIndexes, emptyColumnIndexes);
+  }
+
+  public static bool AreAllEmpty(List<char> list)
+  {
+    return list.All(c => c == '.');
+  }
+}
+
+public class Galaxy
+{
+  public int Y { get; set; }
+  public int X { get; set; }
 }
--- 2023/c#/11Test/UnitTest1.cs
+++ 2023/c#/11Test/UnitTest1.cs
@@ -2,11 +2,132 @@ namespace _11Test;
 
 public class UnitTest1
 {
-  [Fact]
-  public void Test_Sum2()
-  {
-    Assert.Equal(3, Program.Sum2(1, 2));
-    Assert.Equal(0, Program.Sum2(-2, 2));
-    Assert.Equal(-1, Program.Sum2(-2, 1));
-  }
+    [Fact]
+    public void Test_ExpandedSpace()
+    {
+        var ss = new string[]
+        {
+            "...#......",
+            ".......#..",
+            "#.........",
+            "..........",
+            "......#...",
+            ".#........",
+            ".........#",
+            "..........",
+            ".......#..",
+            "#...#.....",
+        };
+        var expextedSS = new string[]
+        {
+            "....#........",
+            ".........#...",
+            "#............",
+            ".............",
+            ".............",
+            "........#....",
+            ".#...........",
+            "............#",
+            ".............",
+            ".............",
+            ".........#...",
+            "#....#.......",
+        };
+        Assert.Equal(expextedSS, Program.ExpandSpace(ss));
+    }
+
+
+    [Fact]
+    public void Test_GetEmptyIndexes()
+    {
+        var input = new string[]
+        {
+            "...#......",
+            ".......#..",
+            "#.........",
+            "..........",
+            "......#...",
+            ".#........",
+            ".........#",
+            "..........",
+            ".......#..",
+            "#...#.....",
+        };
+        var list = input.Select(s => s.ToList()).ToList();
+        var expectedRowIndexes = new List<int> { 3, 7 };
+        var expectedColumnIndexes = new List<int> { 2, 5, 8 };
+        var (a, b) = Program.GetEmptyIndexes(list);
+        Assert.Equal(expectedRowIndexes.ToArray(), a.ToArray());
+        Assert.Equal(expectedColumnIndexes.ToArray(), b.ToArray());
+    }
+
+    [Fact]
+    public void Test_GetGalaxies()
+    {
+        var input = new string[]
+        {
+            ".......",
+            ".#.....",
+            ".....#.",
+            ".......",
+        };
+        var a = new Galaxy();
+        a.Y = 1;
+        a.X = 1;
+
+        var b = new Galaxy();
+        b.Y = 2;
+        b.X = 5;
+
+        var expectedGalaxies = new List<Galaxy> { a, b, };
+
+        var nums = string.Join(":", expectedGalaxies.Select(g => $"{g.Y},{g.X}").ToArray());
+        var nums2 = string.Join(":", Program.GetGalaxies(input).Select(g => $"{g.Y},{g.X}").ToArray());
+
+        Assert.Equal(nums, nums2);
+    }
+
+    [Fact]
+    public void Test_CalculateDistanceSum()
+    {
+        
+        var ss = new string[]
+        {
+            "...#......",
+            ".......#..",
+            "#.........",
+            "..........",
+            "......#...",
+            ".#........",
+            ".........#",
+            "..........",
+            ".......#..",
+            "#...#.....",
+        };
+        Assert.Equal(374, Program.CalculateDistanceSum(ss));
+    }
+
+    [Fact]
+    public void Test_GetDist()
+    {
+        var a = new Galaxy();
+        a.Y = 1;
+        a.X = 1;
+
+        var b = new Galaxy();
+        b.Y = 2;
+        b.X = 5;
+        var dist = (b.Y - a.Y) + (b.X - a.X);
+        Assert.Equal(dist, Program.GetDist(a, b));
+    }
+
+    [Fact]
+    public void Test_AreAllEmpty()
+    {
+        var empty = ".............".ToList();
+        var notEmpty = "........#....".ToList();
+
+        Assert.True(Program.AreAllEmpty(empty));
+        Assert.False(Program.AreAllEmpty(notEmpty));
+    }
 }
