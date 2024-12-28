4eef66ea4a90484612fd74484de17d757cf12884
Sergei Polishsuk <polser1@gmail.com>
2024-12-28 19:17:48 +0200

    09, part2

--- 2024/09.mjs
+++ 2024/09.mjs
@@ -1,4 +1,5 @@
 import { readInput } from '../lib/js/index.mjs'
+import fs from 'fs/promises'
 
 async function main() {
   let input = '2333133121414131402'
@@ -20,32 +21,89 @@ async function main() {
       }
     }
   }
-  // console.log(arr)
 
-  // part1
-  for (let a = 0, b = arr.length - 1; a < b;) {
-    if (arr[a] === '.' && arr[b] !== '.') {
-      let tmp = arr[a]
-      arr[a] = arr[b]
-      arr[b] = tmp
+  // // PART1
+  // for (let a = 0, b = arr.length - 1; a < b;) {
+  //   if (arr[a] === '.' && arr[b] !== '.') {
+  //     let tmp = arr[a]
+  //     arr[a] = arr[b]
+  //     arr[b] = tmp
+  //   }
+  //   // incrementing
+  //   if (arr[a] !== '.') {
+  //     a++
+  //   }
+  //   if (arr[b] === '.') {
+  //     b--
+  //   }
+  // }
+
+
+  // PART 2
+  function swapper(ai, bi, n, arr) {
+    while (n--) {
+      let tmp = arr[ai]
+      arr[ai] = arr[bi]
+      arr[bi] = tmp
+
+      ai++
+      bi++
     }
-    // incrementing
-    if (arr[a] !== '.') {
-      a++
-    }
-    if (arr[b] === '.') {
+  }
+
+  let arrA = []
+  let arrB = []
+  for (
+    let a = 0, b = arr.length - 1;
+    b >= 0;
+  ) {
+    // new element found
+    if (arrB.length && arrB[0] !== arr[b]) {
+
+      // find free space
+      if (arrA.length === arrB.length) {
+        // swap
+        swapper(
+          a - arrA.length,
+          b + 1,
+          arrB.length,
+          arr,
+        )
+
+        arrB = []
+        arrA = []
+        a = 0 // KISSA PASKOO!!!
+
+      } else if (a > b) {
+        // no free space found for current element -> more to next element
+        arrB = []
+        arrA = []
+        a = 0
+      } else if (arr[a] !== '.') {
+        arrA = []
+        a++
+      } else if (arr[a] === '.') {
+        arrA.push(arr[a])
+        a++
+      } else {
+        a++
+      }
+    } else {
+      if (arr[b] !== '.') {
+        arrB.push(arr[b])
+      }
       b--
     }
   }
 
-
-  // console.log(arr.join(''))
+  // console.log('write')
+  // await fs.writeFile('./output.txt', arr.join('|'))
 
   // checksum
   let checksum = 0
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === '.') {
-      break
+      continue
     }
     checksum += arr[i] * i
   }
