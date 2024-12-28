1f10261a41cbe2e81954104aa4d624e769fef8a7
Sergei Polishsuk <polser1@gmail.com>
2024-12-28 02:04:16 +0200

    08 part2

--- 2024/08.mjs
+++ 2024/08.mjs
@@ -13,7 +13,7 @@ async function main() {
 .........A..
 ............
 ............`
-  // input = await readInput('08-input.txt')
+  input = await readInput('08-input.txt')
 
   let rows = input.split('\n').map(x => x.split(''))
 
@@ -39,35 +39,73 @@ async function main() {
   let hashes = {}
 
   function markHash(hash) {
-    if (
-      hash.y >= 0 &&
-      hash.y < rows.length &&
-      hash.x >= 0 &&
-      hash.x < rows[0].length
-    ) {
+    if (checkHashPosition(hash)) {
       hashes[`${hash.y}:${hash.x}`] = true
     }
   }
+
+  function checkHashPosition(hash) {
+    return hash.y >= 0 &&
+      hash.y < rows.length &&
+      hash.x >= 0 &&
+      hash.x < rows[0].length
+  }
+  // part 1
+  // for (let [k, coors] of map) {
+  //   for (let a = 0; a < coors.length - 1; a++) {
+  //     for (let b = a + 1; b < coors.length; b++) {
+  //       let dy = coors[a].y - coors[b].y
+  //       let dx = coors[a].x - coors[b].x
+  //       let hashA = {
+  //         y: coors[a].y + dy,
+  //         x: coors[a].x + dx,
+  //       }
+  //       let hashB = {
+  //         y: coors[b].y - dy,
+  //         x: coors[b].x - dx,
+  //       }
+  //       markHash(hashA)
+  //       markHash(hashB)
+
+  //     }
+  //   }
+  // }
+
+  // part 2
   for (let [k, coors] of map) {
     for (let a = 0; a < coors.length - 1; a++) {
       for (let b = a + 1; b < coors.length; b++) {
         let dy = coors[a].y - coors[b].y
         let dx = coors[a].x - coors[b].x
-        let hashA = {
-          y: coors[a].y + dy,
-          x: coors[a].x + dx,
-        }
-        let hashB = {
-          y: coors[b].y - dy,
-          x: coors[b].x - dx,
-        }
-        markHash(hashA)
-        markHash(hashB)
 
+        let i = 0
+        let hash = {
+          y: coors[a].y,
+          x: coors[a].x,
+        }
+        let positionInMatrix = checkHashPosition(hash)
+        while (positionInMatrix) {
+          markHash(hash)
+          hash.y = hash.y - dy
+          hash.x = hash.x - dx
+          positionInMatrix = checkHashPosition(hash)
+        }
+
+        hash = {
+          y: coors[a].y,
+          x: coors[a].x,
+        }
+        positionInMatrix = checkHashPosition(hash)
+        while (positionInMatrix) {
+          markHash(hash)
+          hash.y = hash.y + dy
+          hash.x = hash.x + dx
+          positionInMatrix = checkHashPosition(hash)
+        }
       }
     }
-
   }
+
 
   console.log(hashes)
   console.log(Object.keys(hashes).length)
